b7e4481bfd1818ca8d19d6ea35e7d5c63a1923a8
Richard Barnes <rlb@ipv.sx>
2018-03-15 12:25:02 -0400

    Add a session interface

---
+++ src/include/session.h
@@ -0,0 +1,53 @@
+#pragma once
+
+#include "common.h" // TODO(rlb@ipv.sx) Locally declare bytes
+#include "state.h"  // TODO(rlb@ipv.sx) Forward declare State
+#include <map>
+
+namespace mls {
+
+class Session
+{
+public:
+  // Create a session joined to an empty group
+  Session(const bytes& group_id, const SignaturePrivateKey& identity_priv);
+
+  // Create an unjoined session
+  Session(const SignaturePrivateKey& identity_priv);
+  Session();
+
+  // Two sessions are considered equal if:
+  // (1) they agree on the states they have in common
+  // (2) they agree on the current epoch
+  friend bool operator==(const Session& lhs, const Session& rhs);
+
+  bytes user_init_key() const;
+  bytes group_init_key() const;
+
+  bytes join(const bytes& group_init_key) const;
+  bytes add(const bytes& user_init_key) const;
+  bytes update() const;
+  bytes remove(uint32_t index) const;
+
+  void handle(const bytes& handshake);
+
+  bytes protect(const bytes& content);
+  bytes unprotect(const bytes& content) const;
+
+  epoch_t current_epoch() const { return _current_epoch; }
+
+private:
+  DHPrivateKey _next_leaf_priv;
+  DHPrivateKey _init_priv;
+  bytes _user_init_key;
+  SignaturePrivateKey _identity_priv;
+  std::map<epoch_t, State> _state;
+  epoch_t _current_epoch;
+
+  void make_init_key();
+  void add_state(const State& state);
+  State& current_state();
+  const State& current_state() const;
+};
+
+} // namespace mls
---
+++ src/session.cpp
@@ -0,0 +1,209 @@
+#include "session.h"
+#include "tls_syntax.h"
+
+namespace mls {
+
+Session::Session(const bytes& group_id,
+                 const SignaturePrivateKey& identity_priv)
+  : _init_priv(DHPrivateKey::generate())
+  , _next_leaf_priv(DHPrivateKey::generate())
+  , _identity_priv(identity_priv)
+{
+  State root(group_id, identity_priv);
+  add_state(root);
+  make_init_key();
+}
+
+Session::Session(const SignaturePrivateKey& identity_priv)
+  : _init_priv(DHPrivateKey::generate())
+  , _next_leaf_priv(DHPrivateKey::generate())
+  , _identity_priv(identity_priv)
+{
+  make_init_key();
+}
+
+Session::Session()
+  : _init_priv(DHPrivateKey::generate())
+  , _next_leaf_priv(DHPrivateKey::generate())
+  , _identity_priv(SignaturePrivateKey::generate())
+{
+  make_init_key();
+}
+
+bool
+operator==(const Session& lhs, const Session& rhs)
+{
+  if (lhs._current_epoch != rhs._current_epoch) {
+    return false;
+  }
+
+  for (const auto& pair : lhs._state) {
+    if (rhs._state.count(pair.first) == 0) {
+      continue;
+    }
+
+    if (rhs._state.at(pair.first) != pair.second) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bytes
+Session::user_init_key() const
+{
+  return _user_init_key;
+}
+
+bytes
+Session::group_init_key() const
+{
+  auto group_init_key = current_state().group_init_key();
+  return tls::marshal(group_init_key);
+}
+
+bytes
+Session::join(const bytes& group_init_key_bytes) const
+{
+  GroupInitKey group_init_key;
+  tls::unmarshal(group_init_key_bytes, group_init_key);
+  auto user_add = State::join(_identity_priv, _init_priv, group_init_key);
+  return tls::marshal(user_add);
+}
+
+bytes
+Session::add(const bytes& user_init_key_bytes) const
+{
+  UserInitKey user_init_key;
+  tls::unmarshal(user_init_key_bytes, user_init_key);
+  auto group_add = current_state().add(user_init_key);
+  return tls::marshal(group_add);
+}
+
+bytes
+Session::update() const
+{
+  auto update = current_state().update(_next_leaf_priv);
+  return tls::marshal(update);
+}
+
+bytes
+Session::remove(uint32_t index) const
+{
+  auto update = current_state().remove(index);
+  return tls::marshal(update);
+}
+
+void
+Session::handle(const bytes& handshake)
+{
+  auto type = HandshakeType(handshake[0]);
+  switch (type) {
+    case HandshakeType::user_add: {
+      Handshake<UserAdd> user_add;
+      tls::unmarshal(handshake, user_add);
+
+      if (_state.size() == 0) {
+        add_state(State(_identity_priv, _init_priv, user_add));
+      } else {
+        add_state(current_state().handle(user_add));
+      }
+    } break;
+
+    case HandshakeType::group_add: {
+      Handshake<GroupAdd> group_add;
+      tls::unmarshal(handshake, group_add);
+
+      if (_state.size() == 0) {
+        add_state(State(_identity_priv, _init_priv, group_add));
+      } else {
+        add_state(current_state().handle(group_add));
+      }
+    } break;
+
+    case HandshakeType::update: {
+      Handshake<Update> update;
+      tls::unmarshal(handshake, update);
+
+      if (update.message.path.back() == _next_leaf_priv.public_key()) {
+        add_state(current_state().handle(update, _next_leaf_priv));
+        _next_leaf_priv = std::move(DHPrivateKey::generate());
+      } else {
+        add_state(current_state().handle(update));
+      }
+
+    } break;
+
+    case HandshakeType::remove: {
+      Handshake<Remove> remove;
+      tls::unmarshal(handshake, remove);
+      add_state(current_state().handle(remove));
+    } break;
+
+    default:
+      throw InvalidMessageTypeError("Unknown HandshakeType");
+  }
+}
+
+bytes
+Session::protect(const bytes& content)
+{
+  auto ciphertext = current_state().protect(content);
+  return tls::marshal(ciphertext);
+}
+
+bytes
+Session::unprotect(const bytes& ciphertext_bytes) const
+{
+  MLSCiphertext ciphertext;
+  tls::unmarshal(ciphertext_bytes, ciphertext);
+
+  if (_state.count(ciphertext.epoch) == 0) {
+    throw MissingStateError("No state available for message epoch");
+  }
+
+  return _state.at(ciphertext.epoch).unprotect(ciphertext);
+}
+
+void
+Session::make_init_key()
+{
+  auto user_init_key = UserInitKey{ _init_priv.public_key() };
+  user_init_key.sign(_identity_priv);
+  _user_init_key = tls::marshal(user_init_key);
+}
+
+void
+Session::add_state(const State& state)
+{
+  // XXX(rlb@ipv.sx) Assumes no epoch collisions
+  _state.emplace(state.epoch(), state);
+
+  // XXX(rlb@ipv.sx) First successor updates the head pointer
+  if (_current_epoch == state.prior_epoch() || _state.size() == 1) {
+    _current_epoch = state.epoch();
+  }
+}
+
+const State&
+Session::current_state() const
+{
+  if (_state.count(_current_epoch) == 0) {
+    throw MissingStateError("No state available for current epoch");
+  }
+
+  return _state.at(_current_epoch);
+}
+
+State&
+Session::current_state()
+{
+  if (_state.count(_current_epoch) == 0) {
+    throw MissingStateError("No state available for current epoch");
+  }
+
+  return _state.at(_current_epoch);
+}
+
+} // namespace mls
