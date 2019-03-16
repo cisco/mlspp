ad5b5bb5dfe59b4b403d9c937f6b4f45baa51ddd
Richard Barnes <rlb@ipv.sx>
2019-03-16 14:40:38 -0400

    Application key schedule test

--- test/test_vectors.cpp
+++ test/test_vectors.cpp
@@ -171,15 +171,15 @@ operator<<(tls::ostream& str, const AppKeyScheduleTestVectors::Step& obj)
 tls::istream&
 operator>>(tls::istream& str, AppKeyScheduleTestVectors& obj)
 {
-  return str >> obj.n_participants >> obj.n_messages >> obj.case_p256 >>
-         obj.case_x25519;
+  return str >> obj.n_participants >> obj.n_messages >> obj.app_secret >>
+         obj.case_p256 >> obj.case_x25519;
 }
 
 tls::ostream&
 operator<<(tls::ostream& str, const AppKeyScheduleTestVectors& obj)
 {
-  return str << obj.n_participants << obj.n_messages << obj.case_p256
-             << obj.case_x25519;
+  return str << obj.n_participants << obj.n_messages << obj.app_secret
+             << obj.case_p256 << obj.case_x25519;
 }
 
 ///
