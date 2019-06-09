sthagen/cisco-mlspp

Don't unnecessarily regenerate states

src/include/state.h
@@ -99,7 +99,7 @@ public:
 
   // Negotiate an initial state with another peer based on their
   // UserInitKey
-  typedef std::pair<State, std::pair<Welcome, MLSPlaintext>> InitialInfo;
+  typedef std::tuple<Welcome, MLSPlaintext, State> InitialInfo;
   static InitialInfo negotiate(
     const bytes& group_id,
     const std::vector<CipherSuite> supported_ciphersuites,
@@ -113,17 +113,20 @@ public:
   ///
 
   // Generate a Add message
-  std::pair<Welcome, MLSPlaintext> add(const UserInitKey& user_init_key) const;
+  std::tuple<Welcome, MLSPlaintext, State> add(
+    const UserInitKey& user_init_key) const;
 
   // Generate an Add message at a specific location
-  std::pair<Welcome, MLSPlaintext> add(uint32_t index,
-                                       const UserInitKey& user_init_key) const;
+  std::tuple<Welcome, MLSPlaintext, State> add(
+    uint32_t index,
+    const UserInitKey& user_init_key) const;
 
   // Generate an Update message (for post-compromise security)
-  MLSPlaintext update(const bytes& leaf_secret);
+  std::tuple<MLSPlaintext, State> update(const bytes& leaf_secret);
 
   // Generate a Remove message (to remove another participant)
-  MLSPlaintext remove(const bytes& evict_secret, uint32_t index) const;
+  std::tuple<MLSPlaintext, State> remove(const bytes& evict_secret,
+                                         uint32_t index) const;
 
   ///
   /// Generic handshake message handler
@@ -226,7 +229,7 @@ private:
 
   // Signing of handshake messages (including creation of the
   // confirmation MAC)
-  MLSPlaintext sign(const GroupOperation& operation) const;
+  std::tuple<MLSPlaintext, State> sign(const GroupOperation& operation) const;
 
   // Signature verification over a handshake message
   bool verify(const MLSPlaintext& pt) const;

src/state.cpp
@@ -193,23 +193,20 @@ State::negotiate(const bytes& group_id,
   // We have manually guaranteed that `suite` is always initialized
   // NOLINTNEXTLINE(clang-analyzer-core.CallAndMessage)
   auto state = State{ group_id, suite, leaf_secret, identity_priv, credential };
-  auto welcome_add = state.add(user_init_key);
-  state = state.handle(welcome_add.second);
-
-  return InitialInfo(state, welcome_add);
+  return state.add(user_init_key);
 }
 
 ///
 /// Message factories
 ///
 
-std::pair<Welcome, MLSPlaintext>
+std::tuple<Welcome, MLSPlaintext, State>
 State::add(const UserInitKey& user_init_key) const
 {
   return add(_tree.size(), user_init_key);
 }
 
-std::pair<Welcome, MLSPlaintext>
+std::tuple<Welcome, MLSPlaintext, State>
 State::add(uint32_t index, const UserInitKey& user_init_key) const
 {
   if (!user_init_key.verify()) {
@@ -224,13 +221,15 @@ State::add(uint32_t index, const UserInitKey& user_init_key) const
   auto welcome_info_str = welcome_info();
   auto welcome =
     Welcome{ user_init_key.user_init_key_id, *pub, welcome_info_str };
+  auto welcome_tuple = std::make_tuple(welcome);
 
   auto welcome_info_hash = welcome_info_str.hash(_suite);
-  auto add = sign(Add{ LeafIndex{ index }, user_init_key, welcome_info_hash });
-  return std::pair<Welcome, MLSPlaintext>(welcome, add);
+  auto add_state =
+    sign(Add{ LeafIndex{ index }, user_init_key, welcome_info_hash });
+  return std::tuple_cat(welcome_tuple, add_state);
 }
 
-MLSPlaintext
+std::tuple<MLSPlaintext, State>
 State::update(const bytes& leaf_secret)
 {
   auto path = _tree.encrypt(_index, leaf_secret);
@@ -238,7 +237,7 @@ State::update(const bytes& leaf_secret)
   return sign(Update{ path });
 }
 
-MLSPlaintext
+std::tuple<MLSPlaintext, State>
 State::remove(const bytes& evict_secret, uint32_t index) const
 {
   if (index >= _tree.size()) {
@@ -554,7 +553,7 @@ sender_data_mask(CipherSuite suite,
   return mask;
 }
 
-MLSPlaintext
+std::tuple<MLSPlaintext, State>
 State::sign(const GroupOperation& operation) const
 {
   auto next = handle(_index, operation);
@@ -567,7 +566,7 @@ State::sign(const GroupOperation& operation) const
   pt.operation = operation;
   pt.operation.value().confirmation = confirm;
   pt.sign(_identity_priv);
-  return pt;
+  return std::make_tuple(pt, next);
 }
 
 bool

cisco/mlspp

src/include/state.h
@@ -100,7 +100,7 @@ public:
 
   // Negotiate an initial state with another peer based on their
   // ClientInitKey
-  typedef std::pair<State, std::pair<Welcome, MLSPlaintext>> InitialInfo;
+  typedef std::tuple<Welcome, MLSPlaintext, State> InitialInfo;
   static InitialInfo negotiate(
     const bytes& group_id,
     const std::vector<CipherSuite> supported_ciphersuites,
@@ -114,19 +114,20 @@ public:
   ///
 
   // Generate a Add message
-  std::pair<Welcome, MLSPlaintext> add(
-    const ClientInitKey& client_init_key) const;
+  std::tuple<Welcome, MLSPlaintext, State> add(
+    const UserInitKey& user_init_key) const;
 
   // Generate an Add message at a specific location
-  std::pair<Welcome, MLSPlaintext> add(
+  std::tuple<Welcome, MLSPlaintext, State> add(
     uint32_t index,
-    const ClientInitKey& client_init_key) const;
+    const UserInitKey& user_init_key) const;
 
   // Generate an Update message (for post-compromise security)
-  MLSPlaintext update(const bytes& leaf_secret);
+  std::tuple<MLSPlaintext, State> update(const bytes& leaf_secret);
 
   // Generate a Remove message (to remove another participant)
-  MLSPlaintext remove(const bytes& leaf_secret, uint32_t index);
+  std::tuple<MLSPlaintext, State> remove(const bytes& leaf_secret,
+                                         uint32_t index) const;
 
   ///
   /// Generic handshake message handler
@@ -230,7 +231,7 @@ private:
 
   // Signing of handshake messages (including creation of the
   // confirmation MAC)
-  MLSPlaintext sign(const GroupOperation& operation) const;
+  std::tuple<MLSPlaintext, State> sign(const GroupOperation& operation) const;
 
   // Signature verification over a handshake message
   bool verify(const MLSPlaintext& pt) const;

src/state.cpp
@@ -189,23 +189,20 @@ State::negotiate(const bytes& group_id,
   // We have manually guaranteed that `suite` is always initialized
   // NOLINTNEXTLINE(clang-analyzer-core.CallAndMessage)
   auto state = State{ group_id, suite, leaf_secret, identity_priv, credential };
-  auto welcome_add = state.add(client_init_key);
-  state = state.handle(welcome_add.second);
-
-  return InitialInfo(state, welcome_add);
+  return state.add(client_init_key);
 }
 
 ///
 /// Message factories
 ///
 
-std::pair<Welcome, MLSPlaintext>
+std::tuple<Welcome, MLSPlaintext, State>
 State::add(const ClientInitKey& client_init_key) const
 {
   return add(_tree.size(), client_init_key);
 }
 
-std::pair<Welcome, MLSPlaintext>
+std::tuple<Welcome, MLSPlaintext, State>
 State::add(uint32_t index, const ClientInitKey& client_init_key) const
 {
   if (!client_init_key.verify()) {
@@ -219,15 +216,16 @@ State::add(uint32_t index, const ClientInitKey& client_init_key) const
 
   auto welcome_info_str = welcome_info();
   auto welcome =
-    Welcome{ client_init_key.client_init_key_id, *pub, welcome_info_str };
+    Welcome{ user_init_key.user_init_key_id, *pub, welcome_info_str };
+  auto welcome_tuple = std::make_tuple(welcome);
 
   auto welcome_info_hash = welcome_info_str.hash(_suite);
-  auto add =
+  auto add_state =
     sign(Add{ LeafIndex{ index }, client_init_key, welcome_info_hash });
-  return std::pair<Welcome, MLSPlaintext>(welcome, add);
+  return std::tuple_cat(welcome_tuple, add_state);
 }
 
-MLSPlaintext
+std::tuple<MLSPlaintext, State>
 State::update(const bytes& leaf_secret)
 {
   auto path = _tree.encrypt(_index, leaf_secret);
@@ -235,7 +233,7 @@ State::update(const bytes& leaf_secret)
   return sign(Update{ path });
 }
 
-MLSPlaintext
+std::tuple<MLSPlaintext, State>
 State::remove(const bytes& leaf_secret, uint32_t index)
 {
   if (index >= _tree.size()) {
@@ -568,7 +566,7 @@ sender_data_aad(const tls::opaque<1>& group_id,
   return w.bytes();
 }
 
-MLSPlaintext
+std::tuple<MLSPlaintext, State>
 State::sign(const GroupOperation& operation) const
 {
   auto pt = MLSPlaintext{ _group_id, _epoch, _index, operation };
@@ -576,7 +574,7 @@ State::sign(const GroupOperation& operation) const
   pt.confirmation =
     hmac(_suite, next._confirmation_key, next._confirmed_transcript_hash);
   pt.sign(_identity_priv);
-  return pt;
+  return std::make_tuple(pt, next);
 }
 
 bool