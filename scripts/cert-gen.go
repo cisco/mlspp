dfef9cc4d59f1e735ba027a1da7d4b8fe081a4f0
Suhas Nandakumar <snandaku@cisco.com>
2020-09-21 00:32:34 -0700

    scripts print results instead of files

--- scripts/cert-gen.go
+++ scripts/cert-gen.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-    "bufio"
 	"crypto"
 	"crypto/ed25519"
 	"crypto/rand"
@@ -10,145 +9,112 @@ import (
 	"math/big"
 	"time"
 	"fmt"
-	"os"
 )
 
-const certFile = "./cert_bundle.bin"
-const caCertFile = "./ca_cert.bin"
-const keyFile = "./key.bin"
-const caKeyFile = "./ca_key.bin"
-
-const DEBUG = false
 
 var (
+  caTemplate = &x509.Certificate{
+    BasicConstraintsValid: true,
+    IsCA:                  true,
+    KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+  }
 
-	caTemplate = &x509.Certificate{
-		BasicConstraintsValid: true,
-		IsCA:                  true,
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-	}
-
-	leafTemplate = &x509.Certificate{
-		BasicConstraintsValid: true,
-		IsCA:                  false,
-		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
-	}
+  leafTemplate = &x509.Certificate{
+    BasicConstraintsValid: true,
+    IsCA:                  false,
+    KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature | x509.KeyUsageCertSign,
+  }
 )
 
 func chk(err error) {
-	if err != nil {
-		panic(err)
-	}
+  if err != nil {
+    panic(err)
+  }
 }
 
 func newEd25519() ed25519.PrivateKey {
-	_, priv, err := ed25519.GenerateKey(rand.Reader)
-	chk(err)
-	return priv
+  _, priv, err := ed25519.GenerateKey(rand.Reader)
+  chk(err)
+  return priv
 }
 
 func makeCert(template, parent *x509.Certificate, parentPriv crypto.Signer, addSKI bool) (crypto.Signer, *x509.Certificate, string) {
-	backdate := time.Hour
-	lifetime := 24 * time.Hour
-	skiSize := 4 // bytes
+  backdate := time.Hour
+  lifetime := 24 * time.Hour
+  skiSize := 4 // bytes
 
-	// Set expiry
-	template.NotBefore = time.Now().Add(-backdate)
-	template.NotAfter = template.NotBefore.Add(lifetime)
+  // Set expiry
+  template.NotBefore = time.Now().Add(-backdate)
+  template.NotAfter = template.NotBefore.Add(lifetime)
 
-	// Set serial number
-	serialNumberLimit := big.NewInt(0).Lsh(big.NewInt(1), 128)
-	serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
-	chk(err)
-	template.SerialNumber = serialNumber
+  // Set serial number
+  serialNumberLimit := big.NewInt(0).Lsh(big.NewInt(1), 128)
+  serialNumber, err := rand.Int(rand.Reader, serialNumberLimit)
+  chk(err)
+  template.SerialNumber = serialNumber
 
-	// Add random SKI if requried
-	template.SubjectKeyId = nil
-	if addSKI {
-		template.SubjectKeyId = make([]byte, skiSize)
-		rand.Read(template.SubjectKeyId)
-	}
+  // Add random SKI if requried
+  template.SubjectKeyId = nil
+  if addSKI {
+    template.SubjectKeyId = make([]byte, skiSize)
+    rand.Read(template.SubjectKeyId)
+  }
 
-	// Generate and parse the certificate
-	priv := parentPriv
-	realParent := template
-	if parent != nil {
-		priv = newEd25519()
-		realParent = parent
-	}
+  // Generate and parse the certificate
+  priv := parentPriv
+  realParent := template
+  if parent != nil {
+    priv = newEd25519()
+    realParent = parent
+  }
 
-	certData, err := x509.CreateCertificate(rand.Reader, template, realParent, priv.Public(), parentPriv)
-	chk(err)
+  certData, err := x509.CreateCertificate(rand.Reader, template, realParent, priv.Public(), parentPriv)
+  chk(err)
 
-    // generate hex version for file storage
-	certHex := make([]byte, hex.EncodedLen(len(certData)))
-    hex.Encode(certHex, certData)
+  // generate hex version for file storage
+  certHex := make([]byte, hex.EncodedLen(len(certData)))
+  hex.Encode(certHex, certData)
 
-	cert, err := x509.ParseCertificate(certData)
-	chk(err)
+  cert, err := x509.ParseCertificate(certData)
+  chk(err)
 
-	return priv, cert, string(certHex)
+  return priv, cert, string(certHex)
 }
 
 
 func makeCertChain(rootPriv crypto.Signer, depth int, addSKI bool) ([]byte, string, []*x509.Certificate,  []string) {
-	chain := make([]*x509.Certificate, depth)
-    chainHex  := make([]string, depth)
+  chain := make([]*x509.Certificate, depth)
+  chainHex  := make([]string, depth)
 
-	_, rootCert, rootHex := makeCert(caTemplate, nil, rootPriv, addSKI)
+  _, rootCert, rootHex := makeCert(caTemplate, nil, rootPriv, addSKI)
 
-	currPriv := rootPriv
-	cert := rootCert
-	certHex := rootHex
-	for i := depth - 1; i > 0; i-- {
-		currPriv, cert, certHex = makeCert(caTemplate, cert, currPriv, addSKI)
-		chain[i] = cert
-		chainHex[i] = certHex
-	}
-
-	currPriv, cert, certHex = makeCert(leafTemplate, cert, currPriv, addSKI)
-	chain[0] = cert
-	chainHex[0] = certHex
-
-	return currPriv.(ed25519.PrivateKey), rootHex, chain, chainHex
-}
-
-func writeToFile(fileName string, data []string) {
-  f, err := os.Create(fileName)
-  chk(err)
-  defer f.Close()
-
-  for _, e := range data {
-    _, err = f.WriteString(e + "\n")
-    chk(err)
+  currPriv := rootPriv
+  cert := rootCert
+  certHex := rootHex
+  for i := depth - 1; i > 0; i-- {
+    currPriv, cert, certHex = makeCert(caTemplate, cert, currPriv, addSKI)
+    chain[i] = cert
+    chainHex[i] = certHex
   }
 
+  currPriv, cert, certHex = makeCert(leafTemplate, cert, currPriv, addSKI)
+  chain[0] = cert
+  chainHex[0] = certHex
+
+  return currPriv.(ed25519.PrivateKey), rootHex, chain, chainHex
 }
 
 func main() {
-    depth := 2
-    rootPriv := newEd25519()
-    writeToFile(caKeyFile, []string {string(rootPriv)})
+  depth := 2
+  rootPriv := newEd25519()
+  fmt.Printf("Root Priv Key %x\n", rootPriv)
 
-    myPriv, rootCertHex, _, chainHex := makeCertChain(rootPriv, depth, false)
-
-    writeToFile(keyFile, []string{string(myPriv)})
-    writeToFile(caCertFile, []string{rootCertHex})
-    writeToFile(certFile, chainHex)
-
-    if DEBUG {
-        f, err := os.Open(certFile)
-            chk(err)
-            scanner := bufio.NewScanner(f)
-            scanner.Split(bufio.ScanLines)
-            for scanner.Scan() {
-                hexCert := scanner.Text()
-                certData, err := hex.DecodeString(hexCert)
-                chk(err)
-                cert, err := x509.ParseCertificate(certData)
-                chk(err)
-                fmt.Printf("Cert: %v\n", cert)
-                fmt.Printf("------------")
-            }
-    }
+  myPriv, rootCertHex, _, chainHex := makeCertChain(rootPriv, depth, false)
+  fmt.Printf("\n Root Certificate %s\n", rootCertHex)
+  fmt.Printf("\n MyPriv %x\n", myPriv)
+  for _, c := range chainHex {
+    fmt.Printf("\n\t --- Begin Certificate ---- \n")
+    fmt.Printf("%s", c)
+    fmt.Printf("\n\t --- End Certificate ---- \n")
+  }
 }
