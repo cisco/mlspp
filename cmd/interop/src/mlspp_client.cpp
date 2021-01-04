f4164febd79a2c21896b53b262191bd5a3737455
Richard Barnes <rlb@ipv.sx>
2021-01-04 17:55:32 -0500

    clang-format

--- cmd/interop/src/mlspp_client.cpp
+++ cmd/interop/src/mlspp_client.cpp
@@ -1,14 +1,14 @@
-#include <memory>
-#include <string>
-#include <sstream>
 #include <iostream>
+#include <memory>
+#include <sstream>
+#include <string>
 
 #include <gflags/gflags.h>
 #include <grpcpp/grpcpp.h>
 
-#include <tls/tls_syntax.h>
 #include <mls/crypto.h>
 #include <mls_vectors/mls_vectors.h>
+#include <tls/tls_syntax.h>
 
 #include "mls_client.grpc.pb.h"
 
@@ -22,19 +22,22 @@ using namespace mls_client;
 static constexpr char implementation_name[] = "mlspp";
 
 static std::string
-bytes_to_string(const std::vector<uint8_t>& data) {
-  return {data.begin(), data.end()};
+bytes_to_string(const std::vector<uint8_t>& data)
+{
+  return { data.begin(), data.end() };
 }
 
 static std::vector<uint8_t>
-string_to_bytes(const std::string& str) {
-  return {str.begin(), str.end()};
+string_to_bytes(const std::string& str)
+{
+  return { str.begin(), str.end() };
 }
 
 class MLSClientImpl final : public MLSClient::Service
 {
   // Map C++ exceptions to gRPC errors
-  Status catch_wrap(std::function<Status()>&& f) {
+  Status catch_wrap(std::function<Status()>&& f)
+  {
     try {
       return f();
     } catch (const std::exception& e) {
@@ -51,9 +54,10 @@ class MLSClientImpl final : public MLSClient::Service
     return Status::OK;
   }
 
-  Status SupportedCiphersuites(ServerContext* /* context */,
-              const SupportedCiphersuitesRequest* /* request */,
-              SupportedCiphersuitesResponse* reply) override
+  Status SupportedCiphersuites(
+    ServerContext* /* context */,
+    const SupportedCiphersuitesRequest* /* request */,
+    SupportedCiphersuitesResponse* reply) override
   {
     reply->clear_ciphersuites();
     for (const auto suite : mls::all_supported_suites) {
@@ -63,15 +67,15 @@ class MLSClientImpl final : public MLSClient::Service
   }
 
   Status GenerateTestVector(ServerContext* /* context */,
-              const GenerateTestVectorRequest* request,
-              GenerateTestVectorResponse* reply) override
+                            const GenerateTestVectorRequest* request,
+                            GenerateTestVectorResponse* reply) override
   {
     return catch_wrap([=]() { return generate_test_vector(request, reply); });
   }
 
   Status VerifyTestVector(ServerContext* /* context */,
-              const VerifyTestVectorRequest* request,
-              VerifyTestVectorResponse* /* reply */) override
+                          const VerifyTestVectorRequest* request,
+                          VerifyTestVectorResponse* /* reply */) override
   {
     return catch_wrap([=]() { return verify_test_vector(request); });
   }
@@ -83,30 +87,30 @@ class MLSClientImpl final : public MLSClient::Service
     auto error = std::optional<std::string>();
     switch (request->test_vector_type()) {
       case TestVectorType::TREE_MATH: {
-          error = tls::get<mls_vectors::TreeMathTestVector>(tv_data).verify();
-          break;
+        error = tls::get<mls_vectors::TreeMathTestVector>(tv_data).verify();
+        break;
       }
 
       case TestVectorType::ENCRYPTION: {
-          error = tls::get<mls_vectors::EncryptionTestVector>(tv_data).verify();
-          break;
+        error = tls::get<mls_vectors::EncryptionTestVector>(tv_data).verify();
+        break;
       }
 
       case TestVectorType::KEY_SCHEDULE: {
-          error = tls::get<mls_vectors::KeyScheduleTestVector>(tv_data).verify();
-          break;
+        error = tls::get<mls_vectors::KeyScheduleTestVector>(tv_data).verify();
+        break;
       }
 
       case TestVectorType::TREEKEM: {
-          auto tv = tls::get<mls_vectors::TreeKEMTestVector>(tv_data);
-          tv.initialize_trees();
-          error = tv.verify();
-          break;
+        auto tv = tls::get<mls_vectors::TreeKEMTestVector>(tv_data);
+        tv.initialize_trees();
+        error = tv.verify();
+        break;
       }
 
       case TestVectorType::MESSAGES: {
-          error = tls::get<mls_vectors::MessagesTestVector>(tv_data).verify();
-          break;
+        error = tls::get<mls_vectors::MessagesTestVector>(tv_data).verify();
+        break;
       }
 
       default:
@@ -121,41 +125,44 @@ class MLSClientImpl final : public MLSClient::Service
   }
 
   Status generate_test_vector(const GenerateTestVectorRequest* request,
-              GenerateTestVectorResponse* reply)
+                              GenerateTestVectorResponse* reply)
   {
     std::vector<uint8_t> tv_data;
     switch (request->test_vector_type()) {
       case TestVectorType::TREE_MATH: {
-          auto tv = mls_vectors::TreeMathTestVector::create(request->n_leaves());
-          tv_data = tls::marshal(tv);
-          break;
+        auto tv = mls_vectors::TreeMathTestVector::create(request->n_leaves());
+        tv_data = tls::marshal(tv);
+        break;
       }
 
       case TestVectorType::ENCRYPTION: {
-          auto suite = static_cast<mls::CipherSuite::ID>(request->cipher_suite());
-          auto tv = mls_vectors::EncryptionTestVector::create(suite, request->n_leaves(), request->n_generations());
-          tv_data = tls::marshal(tv);
-          break;
+        auto suite = static_cast<mls::CipherSuite::ID>(request->cipher_suite());
+        auto tv = mls_vectors::EncryptionTestVector::create(
+          suite, request->n_leaves(), request->n_generations());
+        tv_data = tls::marshal(tv);
+        break;
       }
 
       case TestVectorType::KEY_SCHEDULE: {
-          auto suite = static_cast<mls::CipherSuite::ID>(request->cipher_suite());
-          auto tv = mls_vectors::KeyScheduleTestVector::create(suite, request->n_epochs());
-          tv_data = tls::marshal(tv);
-          break;
+        auto suite = static_cast<mls::CipherSuite::ID>(request->cipher_suite());
+        auto tv = mls_vectors::KeyScheduleTestVector::create(
+          suite, request->n_epochs());
+        tv_data = tls::marshal(tv);
+        break;
       }
 
       case TestVectorType::TREEKEM: {
-          auto suite = static_cast<mls::CipherSuite::ID>(request->cipher_suite());
-          auto tv = mls_vectors::TreeKEMTestVector::create(suite, request->n_leaves());
-          tv_data = tls::marshal(tv);
-          break;
+        auto suite = static_cast<mls::CipherSuite::ID>(request->cipher_suite());
+        auto tv =
+          mls_vectors::TreeKEMTestVector::create(suite, request->n_leaves());
+        tv_data = tls::marshal(tv);
+        break;
       }
 
       case TestVectorType::MESSAGES: {
-          auto tv = mls_vectors::MessagesTestVector::create();
-          tv_data = tls::marshal(tv);
-          break;
+        auto tv = mls_vectors::MessagesTestVector::create();
+        tv_data = tls::marshal(tv);
+        break;
       }
 
       default:
@@ -170,7 +177,7 @@ class MLSClientImpl final : public MLSClient::Service
 DEFINE_uint64(port, 50051, "Port to listen on");
 
 int
-main(int argc, char *argv[])
+main(int argc, char* argv[])
 {
   gflags::ParseCommandLineFlags(&argc, &argv, true);
 
