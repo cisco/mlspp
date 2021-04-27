3a3d201b4b3d42d91bff4a1ef0d84e357b4c8e5b
Franziskus Kiefer <franziskuskiefer@gmail.com>
2021-04-27 16:17:51 +0200

    Fix HPKECiphertext encoding

--- include/mls/crypto.h
+++ include/mls/crypto.h
@@ -96,7 +96,7 @@ struct HPKECiphertext
   bytes ciphertext;
 
   TLS_SERIALIZABLE(kem_output, ciphertext)
-  TLS_TRAITS(tls::vector<2>, tls::vector<4>)
+  TLS_TRAITS(tls::vector<2>, tls::vector<2>)
 };
 
 struct HPKEPublicKey
