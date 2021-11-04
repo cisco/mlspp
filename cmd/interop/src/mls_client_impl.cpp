a32705ccd263e3c7a8b4c6080c27d16a616c2361
Richard L. Barnes <richbarn@cisco.com>
2021-11-04 15:30:24 -0400

    Update interop client

--- cmd/interop/src/mls_client_impl.cpp
+++ cmd/interop/src/mls_client_impl.cpp
@@ -339,6 +339,9 @@ Status
 MLSClientImpl::generate_test_vector(const GenerateTestVectorRequest* request,
                                     GenerateTestVectorResponse* reply)
 {
+  // XXX(RLB): Should this value be set by the test runner?
+  static const uint32_t n_psks = 3;
+
   json j;
   switch (request->test_vector_type()) {
     case TestVectorType::TREE_MATH: {
@@ -356,7 +359,7 @@ MLSClientImpl::generate_test_vector(const GenerateTestVectorRequest* request,
     case TestVectorType::KEY_SCHEDULE: {
       auto suite = static_cast<mls::CipherSuite::ID>(request->cipher_suite());
       j =
-        mls_vectors::KeyScheduleTestVector::create(suite, request->n_epochs());
+        mls_vectors::KeyScheduleTestVector::create(suite, request->n_epochs(), n_psks);
       break;
     }
 
