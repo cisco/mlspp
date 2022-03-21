519c1c3fd8fb370c3600511c50344c15c0754936
Richard Barnes <rlb@ipv.sx>
2022-03-21 16:15:29 -0400

    Remove `friend` declarations

--- src/core_types.cpp
+++ src/core_types.cpp
@@ -200,7 +200,7 @@ LeafNode::verify(CipherSuite cipher_suite,
 
   if (CredentialType::x509 == credential.type()) {
     const auto& cred = credential.get<X509Credential>();
-    if (cred._signature_scheme != tls_signature_scheme(cipher_suite.sig().id)) {
+    if (cred.signature_scheme() != tls_signature_scheme(cipher_suite.sig().id)) {
       throw std::runtime_error("Signature algorithm invalid");
     }
   }
@@ -396,7 +396,7 @@ KeyPackage::verify() const
 
   if (CredentialType::x509 == leaf_node.credential.type()) {
     const auto& cred = leaf_node.credential.get<X509Credential>();
-    if (cred._signature_scheme != tls_signature_scheme(cipher_suite.sig().id)) {
+    if (cred.signature_scheme() != tls_signature_scheme(cipher_suite.sig().id)) {
       throw std::runtime_error("Signature algorithm invalid");
     }
   }
