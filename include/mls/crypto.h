901f4af2b3353365ddd599cc7908c83f3281c268
Richard Barnes <rlb@ipv.sx>
2023-02-07 10:21:12 -0500

    Add support for the P-384 ciphersuite

--- include/mls/crypto.h
+++ include/mls/crypto.h
@@ -51,6 +51,7 @@ struct CipherSuite
     X448_AES256GCM_SHA512_Ed448 = 0x0004,
     P521_AES256GCM_SHA512_P521 = 0x0005,
     X448_CHACHA20POLY1305_SHA512_Ed448 = 0x0006,
+    P384_AES256GCM_SHA384_P384 = 0x0007,
   };
 
   CipherSuite();
@@ -111,7 +112,7 @@ private:
   static const bytes& reference_label();
 };
 
-extern const std::array<CipherSuite::ID, 6> all_supported_suites;
+extern const std::array<CipherSuite::ID, 7> all_supported_suites;
 
 // Utilities
 using hpke::random_bytes;
