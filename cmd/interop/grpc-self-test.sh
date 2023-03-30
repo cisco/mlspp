a2c3da01086c35458cf719f3c23d4ea5083b9b24
Richard Barnes <rlb@ipv.sx>
2023-03-29 21:24:46 -0400

    Update to support the latest external proposal tests

--- cmd/interop/grpc-self-test.sh
+++ cmd/interop/grpc-self-test.sh
@@ -17,17 +17,23 @@ make test-runner/test-runner
 cd ../../../../..
 
 # Run the tests
-find ${INTEROP_DIR}/configs -name "*.json" \
-    | xargs ${INTEROP_DIR}/test-runner/test-runner -client localhost:50001 -public -suite 1 -config \
-    | grep error
-have_error=$?
+have_error=0
+for name in `find ${INTEROP_DIR}/configs -name "*.json"`;
+do
+    echo $name
+    ${INTEROP_DIR}/test-runner/test-runner -client localhost:50001 -public -suite 1 -config $name | grep error
+    if [ $? == 0 ]
+    then
+      have_error=1
+    fi
+done
 
 # Clean up
 kill ${BIN_PID}
 
 # Exit status is flipped from `grep` status, because `grep` succeeds when an
 # error line has been matched.
-if [ $have_error == 0 ]; 
+if [ $have_error == 1 ]; 
 then 
   echo FAIL
   exit 1
