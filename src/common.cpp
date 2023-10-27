d6a5988f94390c9a704e4322997f5ce3d845e774
Richard Barnes <rlb@ipv.sx>
2023-10-27 11:31:49 -0400

    Introduce a trivial change to verify that clang-format CI job sees the right files

--- src/common.cpp
+++ src/common.cpp
@@ -3,8 +3,7 @@
 
 namespace MLS_NAMESPACE {
 
-uint64_t
-seconds_since_epoch()
+uint64_t seconds_since_epoch()
 {
   // TODO(RLB) This should use std::chrono, but that seems not to be available
   // on some platforms.
