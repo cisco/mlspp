787d1cf68c78b0498e4569ddf5592f2b554f9a3b
Richard Barnes <rlb@ipv.sx>
2024-04-10 10:28:35 -0400

    Move tree storage from vector to map.

--- include/mls/treekem.h
+++ include/mls/treekem.h
@@ -119,7 +119,7 @@ struct TreeKEMPublicKey
 {
   CipherSuite suite;
   LeafCount size{ 0 };
-  std::vector<OptionalNode> nodes;
+  std::map<NodeIndex, OptionalNode> nodes;
 
   explicit TreeKEMPublicKey(CipherSuite suite);
 
