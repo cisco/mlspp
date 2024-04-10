0e062becfdc69e3671cdb7bf68dd258aa8e2d522
Richard Barnes <rlb@ipv.sx>
2024-04-10 10:28:35 -0400

    Move tree storage from vector to map.

--- src/tree_math.cpp
+++ src/tree_math.cpp
@@ -47,8 +47,13 @@ LeafCount::full(const LeafCount n)
 }
 
 NodeCount::NodeCount(const LeafCount n)
-  : UInt32(2 * (n.val - 1) + 1)
+  : UInt32(0)
 {
+  if (n.val == 0) {
+    val = 0;
+  } else {
+    val = 2 * (n.val - 1) + 1;
+  }
 }
 
 LeafIndex::LeafIndex(NodeIndex x)
