7d94b1484c4715351384f8858a2de1d28fb92b6e
Rich Logan <rich@richlogan.co.uk>
2024-10-31 20:21:48 +0000

    Openssl dependency cleanup (#426)
    * Move OpenSSL build dependency to libhpke
    
    * Test for WITH_BORINGSSL

---
+++ lib/hpke/test/build.cpp
@@ -0,0 +1,18 @@
+#include <catch2/catch_all.hpp>
+
+TEST_CASE("BoringSSL Define")
+{
+#if defined(__has_include)
+    #if __has_include(<openssl/is_boringssl.h>)
+        #if defined(WITH_BORINGSSL)
+            REQUIRE(WITH_BORINGSSL);
+        #else
+            FAIL("Expect #WITH_BORINGSSL set when compiling with BoringSSL");
+        #endif
+    #else
+        SKIP("Only applicable to BoringSSL");
+    #endif
+#else
+    SKIP("Cannot ensure BoringSSL without __has_include()");
+#endif
+}
