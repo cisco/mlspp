d87cdc68d25840770600a923cb04e6dd6020d509
Richard Barnes <rlb@ipv.sx>
2025-11-19 10:35:25 -0500

    Revert hack changes

--- lib/hpke/src/mlkem.cpp
+++ lib/hpke/src/mlkem.cpp
@@ -286,16 +286,14 @@ get_oqs_kem(KEM::ID kem_id)
   return make_typed_unique(kem_ptr);
 }
 
-// XXX Revert comments here once vcpkg updates their version of boringssl
 static std::tuple<bytes, bytes>
-expand_secret_key(KEM::ID kem_id, const bytes& /* XXX sk */)
+expand_secret_key(KEM::ID kem_id, const bytes& sk)
 {
   auto kem = get_oqs_kem(kem_id);
-  // XXX assert(sk.size() == kem->length_keypair_seed);
+  assert(sk.size() == kem->length_keypair_seed);
   auto expanded_sk = bytes(kem->length_secret_key);
   auto pk = bytes(kem->length_public_key);
-  // XXX const auto rv = kem->keypair_derand(pk.data(), expanded_sk.data(), sk.data());
-  const auto rv = kem->keypair(pk.data(), expanded_sk.data()); // XXX
+  const auto rv = kem->keypair_derand(pk.data(), expanded_sk.data(), sk.data());
   if (rv != OQS_SUCCESS) {
     throw std::runtime_error(std::to_string(rv));
   }
